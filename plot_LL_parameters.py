463c1a6fd2b78b8398f685dde1ebeea2d82aa1ed
cdn-pizarro <ricardo.pizarro@nih.gov>
2023-02-02 17:17:45 -0500

    consolidated the main() function

--- plot_LL_parameters.py
+++ plot_LL_parameters.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pickle
 import matplotlib.pyplot as plt
 from matplotlib import cm
-from shared_core.common_functions import request_path
+from shared_core.common_functions import request_input_path
 
 # getting the name of the directory
 # where the this file is present.
@@ -21,21 +21,23 @@ sys.path.append(parent)
 from IDM_model.src import CDD_functions
 
 
+
 def simulate_estimate_model(fn,gamma0,kappa0,alpha0,verbose=False):
 
 	df = pd.read_csv(fn)
+	# remove practice trials
 	df = df.loc[df['cdd_trial_type']=='task']
 
-	# generate probability based on gamma,kappa and threshold at 0.5
-	p_choose_delay = CDD_functions.probability_choose_delay(
+	# generate probability based on gamma,kappa then threshold at 0.5 to generate choice
+	prob_choice = CDD_functions.probability_choose_delay(
 		df['cdd_immed_amt'],df['cdd_immed_wait'],df['cdd_delay_amt'],df['cdd_delay_wait'],
 		[gamma0,kappa0],[alpha0]*df.shape[0])[0]
-	prob_array = np.around(np.array(p_choose_delay))
+	choice = np.around(np.array(prob_choice))
 
 	# insert probability as choice into data_choice_amt_wait
 	cols = ['cdd_trial_resp.corr','cdd_immed_amt','cdd_immed_wait','cdd_delay_amt','cdd_delay_wait']
 	data_choice_amt_wait = CDD_functions.get_data(df,cols,alpha_hat=alpha0)[0]
-	data_choice_amt_wait['cdd_trial_resp.corr'] = prob_array
+	data_choice_amt_wait['cdd_trial_resp.corr'] = choice
 
 	# estimate parameters based on self-generated data
 	gk_bounds = ((0,8),(1e-3,8))
@@ -54,46 +56,55 @@ def range_variables(v1_bound,v2_bound,nb_samples=100):
 	v2 = np.linspace(v2_bound[0], v2_bound[1], num=nb_samples).tolist()
 	return v1,v2
 
+def plot_save_3D(X,Y,Z,xlabel='',ylabel='',zlabel=''):
 
-def main():
+	fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
-	CDD_fn = request_path(prompt='Please enter the path to an arbitray CDD file')
+	# Plot the surface.
+	X, Y = np.meshgrid(X, Y)
+	surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,linewidth=0, antialiased=False)
+	plt.xlabel(xlabel)
+	plt.ylabel(ylabel)
+	ax.set_zlabel(zlabel)
+
+	pickle_fn = request_save_path(prompt='Please enter the path to the .fig.pickle file to save the plot')
+	# '/Users/pizarror/mturk/figs/model_simulation/negLL_gamma_kappa_2500_samples.fig.pickle'
+	print('Saving 3D plot to : {}'.format(pickle_fn))
+	pickle.dump(fig, open(pickle_fn, 'wb'))
 	
-	# initialize model with a selection of parameters
-	gamma0 = 0.702
-	kappa0 = 0.017
-	alpha0=1
+	plt.show()
+
+
+def simulate_gamma_kappa(fn,nb_samples=50):
+	# set alpha=1, simulate over other variables
+	alpha0 = 1
 
 	# bounds for gamma and kappa
 	gamma_bound = [0,8]
 	kappa_bound = [1e-3,8]
-	# for each variable
-	nb_samples = 50
+
+	# number of samples for each variable
 	gamma,kappa = range_variables(gamma_bound,kappa_bound,nb_samples=nb_samples)
 	negLL = np.zeros((nb_samples,nb_samples))
 
 	for ig,g in enumerate(gamma):
 		print(ig,g)
 		for ik,k in enumerate(kappa):
-			inegLL = simulate_estimate_model(CDD_fn,g,k,alpha0)
+			inegLL = simulate_estimate_model(fn,g,k,alpha0)
 			negLL[ig,ik] = inegLL
-	print(negLL)
 
-	fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+	return gamma,kappa,negLL
 
-	# Plot the surface.
-	gamma, kappa = np.meshgrid(gamma, kappa)
-	surf = ax.plot_surface(gamma, kappa, negLL, cmap=cm.coolwarm,
-		linewidth=0, antialiased=False)
-	plt.xlabel('gamma')
-	plt.ylabel('kappa')
-	ax.set_zlabel('negative log-likelihood')
 
-	pickle_fn = '/Users/pizarror/mturk/figs/model_simulation/negLL_gamma_kappa_2500_samples.fig.pickle'
-	print('Saving 3D plot to : {}'.format(pickle_fn))
-	pickle.dump(fig, open(pickle_fn, 'wb'))
+def main():
+
+	CDD_fn = request_input_path(prompt='Please enter the path to an arbitray CDD file')
 	
-	plt.show()
+	gamma,kappa,negLL = simulate_gamma_kappa(CDD_fn,nb_samples=50)
+
+	plot_save_3D(gamma,kappa,negLL,xlabel='gamma',ylabel='kappa',zlabel='negative log-likelihood')
+
+
 
 	'''
 	===Showing the figure after saving it===
