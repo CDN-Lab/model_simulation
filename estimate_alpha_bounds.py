bdafdf03c8091a6dbef15ef15ecc3a3598969565
cdn-pizarro <ricardo.pizarro@nih.gov>
2023-02-09 11:32:20 -0500

    similar to kappa script but for CRDMs alpha

---
+++ estimate_alpha_bounds.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os,sys
+import math
+from shared_core.common_functions import make_dir, request_input_path, save_df
+
+
+def main():
+	# take an arbitrary subject for example
+	# '/Users/pizarror/mturk/idm_data/split/idm_2022-12-08_12h53.16.781/crdm/idm_2022-12-08_12h53.16.781_crdm.csv'
+	CRDM_fn = request_input_path(prompt='Please enter the path to an arbitray CRDM file')
+	df = pd.read_csv(CRDM_fn)
+	df['crdm_lott_amt'] = df['crdm_lott_top'] + df['crdm_lott_bot']
+	df = df.loc[df['crdm_amb_lev']==0].reset_index(drop=True)
+	print(df)
+	print(list(df))
+
+	# indifference point for each possible set of task values, set SV_now = SV_later to find alpha
+	# alpha = log(p_risk) / (log(v_safe/v_risk)) 
+	df['alpha'] = math.log( df['crdm_lott_p'] ) / math.log( df['crdm_sure_amt']/df['crdm_lott_amt'] )
+	df = df.sort_values(by=['alpha'])
+	df_alpha = df[['alpha','crdm_sure_amt', 'crdm_sure_p', 'crdm_lott_amt', 'crdm_lott_p','crdm_amb_lev']]
+	print(df_alpha)
+
+	save_df(df_alpha,prompt='Please enter the path where to write alpha estimates')
+
+
+if __name__ == "__main__":
+	# main will be executed after running the script
+    main()
