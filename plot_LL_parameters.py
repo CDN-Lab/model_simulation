74cc064e736d20da961c2a234379b96b1825f4fc
cdn-pizarro <ricardo.pizarro@nih.gov>
2023-02-23 17:16:11 -0500

    added plots to visualize parameter estimation

--- plot_LL_parameters.py
+++ plot_LL_parameters.py
@@ -61,20 +61,26 @@ def simulate_estimate_CDD_model(index,fn,gamma0,kappa0,alpha0,verbose=False):
 	# generate probability based on gamma,kappa then threshold at 0.5 to generate choice
 	p_choose_reward,SV_null,SV_reward = mf.probability_choice([gamma0,kappa0],data['cdd_immed_amt'],data['cdd_delay_amt'],
 		time_null=data['cdd_immed_wait'],time_reward=data['cdd_delay_wait'],alpha=data['alpha'],task='cdd')
-	SV_delta = [rew-null for (rew,null) in zip(SV_reward,SV_null)]
-	
 	# print(np.around(np.array(prob_choice)))
-	data['cdd_trial_resp.corr'] = np.around(np.array(p_choose_reward))
+	p_array = np.array(p_choose_reward)
+	rand_array = np.random.normal(0.0,0.2,p_array.shape)
+	choice = np.around(p_array+rand_array)
+	choice[choice==2]=1
+	choice[choice==-1]=0
+	data['cdd_trial_resp.corr'] = choice
 
 	# estimate parameters based on self-generated data
-	gk_guess = [0.15, 0.5]
-	gk_bounds = ((0,8),(0.0022,0.368))
+	gk_guess = [0.15, 0.005]
+	gk_bounds = ((0,6),(0.0022,0.368))
 	negLL,gamma_hat,kappa_hat = mf.fit_computational_model(data,guess=gk_guess,bounds=gk_bounds,disp=verbose)
+	print('Kappa Hat : {}'.format(kappa_hat))
 
 	# sorted for plotting
+	SV_delta = [rew-null for (rew,null) in zip(SV_reward,SV_null)]	
 	SV_delta, p_choose_reward = zip(*sorted(zip(SV_delta, p_choose_reward)))
 	plt = mf.plot_fit(index,SV_delta,p_choose_reward,choice=data['cdd_trial_resp.corr'].tolist(),ylabel='prob_choose_delay',xlabel='SV difference (SV_delay - SV_immediate)',
 		title=r'$\gamma={0:0.3f}, \kappa={1:0.3f}$'.format(gamma0,kappa0))
+	print('Kappa Hat : {}'.format(kappa_hat))
 	textstr = r'$(\hat \gamma,\hat \kappa) : ({0:0.3f},{1:0.3f})$'.format(gamma_hat,kappa_hat)
 	# these are matplotlib.patch.Patch properties
 	props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
@@ -89,12 +95,13 @@ def simulate_estimate_CDD_model(index,fn,gamma0,kappa0,alpha0,verbose=False):
 	print('Saving to : {}'.format(fig_fn))
 	plt.close(index)
 
+	print('Kappa Hat : {}'.format(kappa_hat))
 	print('Ground truth for (gamma,kappa) : ({},{})'.format(gamma0,kappa0))
 	print('Estimated values (gamma,kappa) : ({},{})'.format(gamma_hat,kappa_hat))
 	if verbose:
 		print(data)
 		print("Negative log-likelihood: {}, gamma: {}, kappa: {}". format(negLL, gamma_hat, kappa_hat))
-	return negLL
+	return negLL,gamma_hat,kappa_hat
 
 def simulate_estimate_CRDM_model(index,fn,gamma0,alpha0,beta0,verbose=False):
 	df = pd.read_csv(fn)
@@ -124,8 +131,29 @@ def simulate_estimate_CRDM_model(index,fn,gamma0,alpha0,beta0,verbose=False):
 def range_variables(v1_bound,v2_bound,nb_samples=100):
 
 	v1 = np.linspace(v1_bound[0], v1_bound[1], num=nb_samples).tolist()
-	v2 = np.linspace(v2_bound[0], v2_bound[1], num=nb_samples).tolist()
+	v2 = np.linspace(v2_bound[0], v2_bound[1], num=2*nb_samples).tolist()
 	return v1,v2
+
+
+def plot_ground_hat(v1_ground,v2_ground,v1_hat,v2_hat):
+	plt.figure(1000)
+	print(v1_hat)
+	for i in range(v2_ground.shape[1]):
+		plt.plot(v1_ground[:,i],v1_hat[:,i],'*-',label=r'$\kappa = {0:0.3f}$'.format(v2_ground[3,i]))
+		plt.xlabel(r'$\gamma_{truth}$',fontsize=12)
+		plt.ylabel(r'$\gamma_{estimate}$',fontsize=12)
+	plt.legend(loc='center left',bbox_to_anchor=(1, 0.5))
+	plt.tight_layout()
+	plt.figure(1001)
+	print(v2_hat)
+	for i in range(v1_ground.shape[0]):
+		plt.plot(np.log(v2_ground[i,:]),np.log(v2_hat[i,:]),'*-',label=r'$\gamma = {0:0.3f}$'.format(v1_ground[i,0]))
+		plt.xlabel(r'$\kappa_{truth}$',fontsize=12)
+		plt.ylabel(r'$\kappa_{estimate}$',fontsize=12)
+	plt.legend(loc='center left',bbox_to_anchor=(1, 0.5))
+	plt.tight_layout()
+	plt.show()
+	sys.exit()
 
 
 def simulate_v1_v2(task='CDD',fn='',v1_bound=[0,8],v2_bound=[1e-3,8],v_fixed=1.0,nb_samples=50):
@@ -133,15 +161,26 @@ def simulate_v1_v2(task='CDD',fn='',v1_bound=[0,8],v2_bound=[1e-3,8],v_fixed=1.0
 
 	# prepare the variables to range and negLL matrix for storing values
 	var1,var2 = range_variables(v1_bound,v2_bound,nb_samples=nb_samples)
-	negLL = np.zeros((nb_samples,nb_samples))
+	xsize,ysize = len(var1),len(var2)
+	negLL = np.zeros((xsize,ysize))
+	v1_ground = np.zeros((xsize,ysize))
+	v1_hat = np.zeros((xsize,ysize))
+	v2_ground = np.zeros((xsize,ysize))
+	v2_hat = np.zeros((xsize,ysize))
 	index = 0
 	if 'CDD' in task:
 		for iv1,v1 in enumerate(var1):
 			print(iv1,v1)
 			for iv2,v2 in enumerate(var2):
+					print(iv2,v2)
 					v2 = np.exp(v2)
-					negLL[iv1,iv2] = simulate_estimate_CDD_model(index,fn,v1,v2,v_fixed)
+					print(iv1,iv2,v1)
+					print(iv1,iv2,v2)
+					v1_ground[iv1,iv2] = v1
+					v2_ground[iv1,iv2] = v2
+					negLL[iv1,iv2],v1_hat[iv1,iv2],v2_hat[iv1,iv2] = simulate_estimate_CDD_model(index,fn,v1,v2,v_fixed)
 					index += 1
+		plot_ground_hat(v1_ground,v2_ground,v1_hat,v2_hat)
 	elif 'CRDM' in task:
 		for iv1,v1 in enumerate(var1):
 			print(iv1,v1)
@@ -169,12 +208,12 @@ def simulate_CDD(nb_samples=50):
 	# First simulation, fix alpha to 1.0 and vary gamma and kappa
 	alpha0 = 1
 	# bounds for gamma and kappa : (noise and discount rate)
-	gamma_bound = [0,8]
+	gamma_bound = [0,5]
 	# range for ln(discount_rate) : [-6,-1]
 	# kappa_bound = [0.0022,0.368]
-	log_discount_rate = [-6,-1]
+	log_discount_rate_bound = [-6,-1]
 	
-	gamma,kappa,negLL = simulate_v1_v2(task=task,fn=CDD_fn,v1_bound=gamma_bound,v2_bound=kappa_bound,v_fixed=alpha0,nb_samples=nb_samples)
+	gamma,kappa,negLL = simulate_v1_v2(task=task,fn=CDD_fn,v1_bound=gamma_bound,v2_bound=log_discount_rate_bound,v_fixed=alpha0,nb_samples=nb_samples)
 	# kappa = np.log(kappa)
 	plot_save_3D(gamma,kappa,negLL,xlabel='gamma',ylabel='kappa',zlabel='negative log-likelihood',nb_samples=nb_samples,verbose=False)
 
@@ -201,7 +240,7 @@ def simulate_CRDM(nb_samples=50):
 	
 def main():
 	# For some reason I cannot run these together, I have to run for one task, save, and rerun script
-	nb_samples=10
+	nb_samples=5
 
 	simulate_CDD(nb_samples=nb_samples)
 	# simulate_CRDM(nb_samples=nb_samples)
