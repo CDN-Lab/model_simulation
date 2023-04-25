151698a1ee10de0f266098e1d4dc7e7007a8fb63
cdn-pizarro <ricardo.pizarro@nih.gov>
2023-04-25 11:03:53 -0400

    trying to debug CRDM parameter recovery

--- plot_LL_parameters.py
+++ plot_LL_parameters.py
@@ -63,7 +63,7 @@ def simulate_estimate_CDD_model(index,fn,gamma0,kappa0,alpha0,verbose=False):
 	# print(np.around(np.array(prob_choice)))
 
 	p_array = np.array(p_choose_reward)
-	rand_array = np.random.normal(0.0,0.1,p_array.shape)
+	# rand_array = np.random.normal(0.0,0.1,p_array.shape)
 	bar = np.random.uniform(0,1,p_array.shape)
 	# choice = np.around(p_array)#+rand_array)
 	choice = p_array > bar
@@ -120,20 +120,58 @@ def simulate_estimate_CRDM_model(index,fn,gamma0,alpha0,beta0,verbose=False):
 	data = mf.get_data(df,cols)[0]
 
 	# generate probability based on gamma,kappa then threshold at 0.5 to generate choice and insert into data
-	prob_choice = mf.probability_choice([gamma0,beta0,alpha0],data['crdm_sure_amt'],data['crdm_lott_amt'],
-		p_null=data['crdm_sure_p'],p_reward=data['crdm_lott_p'],ambiguity=data['crdm_amb_lev'],task='crdm')[0]
-	data['crdm_trial_resp.corr'] = np.around(np.array(prob_choice))
+	prob_choose_lott,SV_null,SV_reward = mf.probability_choice([gamma0,beta0,alpha0],data['crdm_sure_amt'],data['crdm_lott_amt'],
+		p_null=data['crdm_sure_p'],p_reward=data['crdm_lott_p'],ambiguity=data['crdm_amb_lev'],task='crdm')
+	# data['crdm_trial_resp.corr'] = np.around(np.array(prob_choice))
+
+	p_array = np.array(prob_choose_lott)
+	# rand_array = np.random.normal(0.0,0.1,p_array.shape)
+	bar = np.random.uniform(0,1,p_array.shape)
+	# choice = np.around(p_array)#+rand_array)
+	choice = p_array > bar
+	choice[choice==2]=1 
+	choice[choice==-1]=0
+	data['crdm_trial_resp.corr'] = choice
+
+	# sorted for plotting
+	SV_delta = [rew-null for (rew,null) in zip(SV_reward,SV_null)]
+	# print(choice)
+	SV_delta, prob_choose_lott,choice = (list(t) for t in zip(*sorted(zip(SV_delta, prob_choose_lott,choice))))
 
 	# estimate parameters based on self-generated data
 	gba_guess = [0.15, 0.5, 0.6]
 	gba_bounds = ((0,8),(1e-8,6.4),(0.125,4.341))
 	negLL,gamma_hat,beta_hat,alpha_hat = mf.fit_computational_model(data,guess=gba_guess,bounds=gba_bounds,disp=verbose)
+
+	print('Alpha Hat : {}'.format(alpha_hat))
+
+	plt = mf.plot_fit(index,SV_delta,prob_choose_lott,choice=choice,ylabel='prob_choose_delay',xlabel='SV difference (SV_delay - SV_immediate)',
+		title=r'$\gamma={0:0.3f}, \alpha={1:0.3f}$'.format(gamma0,alpha0))
+	# plt.plot(SV_delta,p_choose_reward,'g^:',linewidth=0.5)
+	# sys.exit()
+	print('Alpha Hat : {}'.format(alpha_hat))
+	textstr = r'$(\hat \gamma,\hat \kappa) : ({0:0.3f},{1:0.3f})$'.format(gamma_hat,alpha_hat)
+	# these are matplotlib.patch.Patch properties
+	props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+	# place a text box in upper left in axes coords
+	xpos = 0.5*(max(SV_delta) - min(SV_delta)) + min(SV_delta)
+	plt.text(xpos, 0.8, textstr, fontsize=14,verticalalignment='top', bbox=props)
+
+	model_sim_dir = '/Users/pizarror/mturk/model_simulation/figs/choice_fit'	
+	cf.make_dir(model_sim_dir)
+	fig_fn = os.path.join(model_sim_dir,'gamma_{0:0.4f}_alpha_{1:0.4f}.png'.format(gamma0,alpha0))
+	plt.savefig(fig_fn)
+	print('Saving to : {}'.format(fig_fn))
+	plt.close(index)
+
+	print('Alpha Hat : {}'.format(alpha_hat))
+
 	print('Ground truth for (gamma,alpha,beta) : ({},{},{})'.format(gamma0,alpha0,beta0))
 	print('Estimated values (gamma,alpha,beta) : ({},{},{})'.format(gamma_hat,alpha_hat,beta_hat))
 	if verbose:
 		print(data)
 		print("Negative log-likelihood: {}, gamma: {}, beta: {}, alpha: {}". format(negLL, gamma_hat, beta_hat, alpha_hat))
-	return negLL
+	return negLL, gamma_hat,alpha_hat
 
 
 def range_variables(v1_bound,v2_bound,nb_samples=100):
@@ -144,22 +182,33 @@ def range_variables(v1_bound,v2_bound,nb_samples=100):
 
 
 def plot_ground_hat(v1_ground,v2_ground,v1_hat,v2_hat):
+
 	plt.figure(1000)
 	print(v1_hat)
 	for i in range(v2_ground.shape[1]):
-		plt.plot(v1_ground[:,i],v1_hat[:,i],'*-',label=r'$\kappa = {0:0.3f}$'.format(v2_ground[3,i]))
+		plt.plot(v1_ground[:,i],v1_hat[:,i],'*-',label=r'$\alpha = {0:0.3f}$'.format(v2_ground[3,i]))
 		plt.xlabel(r'$\gamma_{truth}$',fontsize=12)
 		plt.ylabel(r'$\gamma_{estimate}$',fontsize=12)
 	plt.legend(loc='center left',bbox_to_anchor=(1, 0.5))
 	plt.tight_layout()
+
 	plt.figure(1001)
 	print(v2_hat)
 	for i in range(v1_ground.shape[0]):
-		plt.plot(np.log(v2_ground[i,:]),np.log(v2_hat[i,:]),'*-',label=r'$\gamma = {0:0.3f}$'.format(v1_ground[i,0]))
-		plt.xlabel(r'$\kappa_{truth}$',fontsize=12)
-		plt.ylabel(r'$\kappa_{estimate}$',fontsize=12)
+		plt.plot(v2_ground[i,:],v2_hat[i,:],'*-',label=r'$\gamma = {0:0.3f}$'.format(v1_ground[i,0]))
+		plt.xlabel(r'$\alpha_{truth}$',fontsize=12)
+		plt.ylabel(r'$\alpha_{estimate}$',fontsize=12)
 	plt.legend(loc='center left',bbox_to_anchor=(1, 0.5))
 	plt.tight_layout()
+
+	# plt.figure(1001)
+	# print(v2_hat)
+	# for i in range(v1_ground.shape[0]):
+	# 	plt.plot(np.log(v2_ground[i,:]),np.log(v2_hat[i,:]),'*-',label=r'$\gamma = {0:0.3f}$'.format(v1_ground[i,0]))
+	# 	plt.xlabel(r'$\kappa_{truth}$',fontsize=12)
+	# 	plt.ylabel(r'$\kappa_{estimate}$',fontsize=12)
+	# plt.legend(loc='center left',bbox_to_anchor=(1, 0.5))
+	# plt.tight_layout()
 	# plt.show()
 	# sys.exit()
 
@@ -193,8 +242,15 @@ def simulate_v1_v2(task='CDD',fn='',v1_bound=[0,8],v2_bound=[1e-3,8],v_fixed=1.0
 		for iv1,v1 in enumerate(var1):
 			print(iv1,v1)
 			for iv2,v2 in enumerate(var2):
-					negLL[iv1,iv2] = simulate_estimate_CRDM_model(index,fn,v1,v2,v_fixed)
+					print(iv2,v2)
+					# v2 = np.exp(v2)
+					print(iv1,iv2,v1)
+					print(iv1,iv2,v2)
+					v1_ground[iv1,iv2] = v1
+					v2_ground[iv1,iv2] = v2
+					negLL[iv1,iv2],v1_hat[iv1,iv2],v2_hat[iv1,iv2] = simulate_estimate_CRDM_model(index,fn,v1,v2,v_fixed)
 					index += 1
+		plot_ground_hat(v1_ground,v2_ground,v1_hat,v2_hat)
 	else:
 		print('No task selected')
 
@@ -224,7 +280,7 @@ def simulate_CDD(nb_samples=50):
 	
 	gamma,kappa,negLL = simulate_v1_v2(task=task,fn=CDD_fn,v1_bound=gamma_bound,v2_bound=log_discount_rate_bound,v_fixed=alpha0,nb_samples=nb_samples)
 	# kappa = np.log(kappa)
-	plot_save_3D(gamma,kappa,negLL,xlabel='gamma',ylabel='kappa',zlabel='negative log-likelihood',nb_samples=nb_samples,verbose=False)
+	# plot_save_3D(gamma,kappa,negLL,xlabel='gamma',ylabel='kappa',zlabel='negative log-likelihood',nb_samples=nb_samples,verbose=False)
 
 	fn='estimates/cdd_gkLL.npy'
 	save_to_numpy(fn,gamma,kappa,negLL)
@@ -243,7 +299,7 @@ def simulate_CRDM(nb_samples=50):
 	alpha_bound = [0.125,4.341]
 
 	gamma,alpha,negLL = simulate_v1_v2(task=task,fn=CRDM_fn,v1_bound=gamma_bound,v2_bound=alpha_bound,v_fixed=beta0,nb_samples=nb_samples)
-	plot_save_3D(gamma,alpha,negLL,xlabel='gamma',ylabel='alpha',zlabel='negative log-likelihood',nb_samples=nb_samples,verbose=False)
+	# plot_save_3D(gamma,alpha,negLL,xlabel='gamma',ylabel='alpha',zlabel='negative log-likelihood',nb_samples=nb_samples,verbose=False)
 	
 	fn='estimates/crdm_gaLL.npy'
 	save_to_numpy(fn,gamma,alpha,negLL)
