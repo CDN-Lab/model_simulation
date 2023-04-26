e11788e40a986836b3cdff9736b0ffb148cbb2dc
cdn-pizarro <ricardo.pizarro@nih.gov>
2023-04-26 15:28:48 -0400

    estimate LL for simulated data instead of modeling

--- CDD_parameter_recovery.py
+++ CDD_parameter_recovery.py
@@ -21,22 +21,28 @@ sys.path.append(parent)
 from IDM_model.src import model_functions as mf
 
 
-def plot_save_3D(X,Y,Z,xlabel='',ylabel='',zlabel='',nb_samples=50,verbose=False):
-
+def plot_save_3D(X,Y,Z,c0,c_hat,xlabel='',ylabel='',zlabel='',nb_samples=50,verbose=False):
 	fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-
+	print(c_hat)
+	print(c0)
 	# Plot the surface.
-	X, Y = np.meshgrid(X, Y)
-	surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,linewidth=0, antialiased=False)
+	Xmesh, Ymesh = np.meshgrid(X, Y)
+	print('ground truth')
+	print(X[c0[0]], Y[c0[1]], Z[c0[0],c0[1]])
+	print('estimate')
+	print(X[c_hat[0]], Y[c_hat[1]], Z[c_hat[0],c_hat[1]])
+	ax.plot_surface(Xmesh, Ymesh, Z, cmap=cm.coolwarm,linewidth=0, antialiased=False)
+	ax.scatter(Xmesh[c0[0],c0[1]], Ymesh[c0[0],c0[1]], Z[c0[0],c0[1]], c='blue', marker='+', s=500)
+	ax.scatter(Xmesh[c_hat[0],c_hat[1]], Ymesh[c_hat[0],c_hat[1]], Z[c_hat[0],c_hat[1]], c='red', marker='*', s=1000)
 	plt.xlabel(xlabel)
 	plt.ylabel(ylabel)
 	ax.set_zlabel(zlabel)
 
-	model_sim_dir = '/Users/pizarror/mturk/model_simulation/figs'
-	pickle_fn = os.path.join(model_sim_dir,'negLL_{}_{}_{}_samples.fig.pickle'.format(xlabel,ylabel,int(nb_samples**2)))
+	# model_sim_dir = '/Users/pizarror/mturk/model_simulation/figs'
+	# pickle_fn = os.path.join(model_sim_dir,'negLL_{}_{}_{}_samples.fig.pickle'.format(xlabel,ylabel,int(nb_samples**2)))
 	# request_save_path(prompt='Please enter the path to the .fig.pickle file to save the plot')
-	print('Saving 3D plot to : {}'.format(pickle_fn))
-	pickle.dump(fig, open(pickle_fn, 'wb'))
+	# print('Saving 3D plot to : {}'.format(pickle_fn))
+	# pickle.dump(fig, open(pickle_fn, 'wb'))
 
 	if verbose:
 		print('\n\n===Showing the figure in python (or ipython) after saving it===\n\n')
@@ -48,7 +54,7 @@ def plot_save_3D(X,Y,Z,xlabel='',ylabel='',zlabel='',nb_samples=50,verbose=False
 
 
 
-def estimate_NLL_CDD_model(data,gamma0,kappa0):
+def estimate_NLL_model(data,gamma0,kappa0):
 	# estimate parameters based on self-generated data
 	parms=[gamma0,kappa0]
 	negLL = mf.function_negLL(parms,data)
@@ -84,7 +90,7 @@ def plot_ground_hat(v1_ground,v2_ground,v1_hat,v2_hat):
 	plt.show()
 	sys.exit()
 
-def simulate_data(fn,alpha0,gamma0,kappa0):
+def simulate_data(fn,alpha0,gamma0=0.8,kappa0=0.5):
 	df = pd.read_csv(fn)
 	# remove practice trials
 	df = df.loc[df['cdd_trial_type']=='task']
@@ -109,34 +115,19 @@ def simulate_data(fn,alpha0,gamma0,kappa0):
 
 	return data
 
-def simulate_v1_v2(fn='',v1_bound=[0,8],v2_bound=[1e-3,8],v_fixed=1.0,nb_samples=50):
+def simulate_v1_v2(fn='',v1_0=0.8,v2_0=0.5,v_fixed=1.0,v1_bound=[0,8],v2_bound=[1e-3,8],nb_samples=50):
 	# nb_samples is number of samples for each variable
 
 	# simulate data
-	data = simulate_data(fn,alpha0=1,gamma0=0.8,kappa0=0.5)
-
+	data = simulate_data(fn,alpha0=v_fixed,gamma0=v1_0,kappa0=v2_0)
 	# prepare the variables to range and negLL matrix for storing values
 	var1,var2 = range_variables(v1_bound,v2_bound,nb_samples=nb_samples)
 	xsize,ysize = len(var1),len(var2)
 	negLL = np.zeros((xsize,ysize))
-	v1_ground = np.zeros((xsize,ysize))
-	v1_hat = np.zeros((xsize,ysize))
-	v2_ground = np.zeros((xsize,ysize))
-	v2_hat = np.zeros((xsize,ysize))
-	index = 0
 	for iv1,v1 in enumerate(var1):
-		print(iv1,v1)
 		for iv2,v2 in enumerate(var2):
-				print(iv2,v2)
 				v2 = np.exp(v2)
-				print(iv1,iv2,v1)
-				print(iv1,iv2,v2)
-				v1_ground[iv1,iv2] = v1
-				v2_ground[iv1,iv2] = v2
-				negLL[iv1,iv2],v1_hat[iv1,iv2],v2_hat[iv1,iv2] = estimate_NLL_CDD_model(index,data,v1,v2,v_fixed)
-				index += 1
-	plot_ground_hat(v1_ground,v2_ground,v1_hat,v2_hat)
-
+				negLL[iv1,iv2] = estimate_NLL_model(data,v1,v2)
 	return var1,var2,negLL
 
 
@@ -145,6 +136,11 @@ def save_to_numpy(fn,gamma,kappa,negLL):
 		np.save(f, gamma)
 		np.save(f, kappa)
 		np.save(f, negLL)
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
 
 def simulate_CDD(nb_samples=50):
 	CDD_fn = '/Users/pizarror/mturk/idm_data/batch_output/bonus2/idm_2022-12-08_14h39.52.884/cdd/idm_2022-12-08_14h39.52.884_cdd.csv'
@@ -158,10 +154,19 @@ def simulate_CDD(nb_samples=50):
 	# range for ln(discount_rate) : [-6,-1]
 	# kappa_bound = [0.0022,0.368]
 	log_discount_rate_bound = [-8,1]
-	
-	gamma,kappa,negLL = simulate_v1_v2(fn=CDD_fn,v1_bound=gamma_bound,v2_bound=log_discount_rate_bound,v_fixed=alpha0,nb_samples=nb_samples)
-	# kappa = np.log(kappa)
-	# plot_save_3D(gamma,kappa,negLL,xlabel='gamma',ylabel='kappa',zlabel='negative log-likelihood',nb_samples=nb_samples,verbose=False)
+	# ground truth
+	gamma0=0.8
+	kappa0=0.5
+
+	gamma,kappa,negLL = simulate_v1_v2(fn=CDD_fn,v1_0=gamma0,v2_0=kappa0,v_fixed=alpha0,v1_bound=gamma_bound,v2_bound=log_discount_rate_bound,nb_samples=nb_samples)
+	(row,col) = np.where(negLL == np.min(negLL))
+	row0 = find_nearest(gamma,gamma0)
+	col0 = find_nearest(kappa,np.log(kappa0))
+	coords0 = (row0,col0)
+	print('Ground truth for (gamma,kappa): ({0},{1})'.format(gamma0,np.log(kappa0)))
+	print('Min of negLL for (gamma,kappa): ({0:0.3f},{1:0.3f})'.format(gamma[row[0]],kappa[col[0]]))
+	coords_hat = (row[0],col[0])
+	plot_save_3D(gamma,kappa,negLL,coords0,coords_hat,xlabel='gamma',ylabel='kappa',zlabel='negative log-likelihood',nb_samples=nb_samples,verbose=False)
 
 	fn='estimates/cdd_gkLL.npy'
 	save_to_numpy(fn,gamma,kappa,negLL)
@@ -170,7 +175,6 @@ def simulate_CDD(nb_samples=50):
 def main():
 	# For some reason I cannot run these together, I have to run for one task, save, and rerun script
 	nb_samples=50
-
 	simulate_CDD(nb_samples=nb_samples)
 
 
